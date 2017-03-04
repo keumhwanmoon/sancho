4156ea01f6fe1da816b1c9d386a2ce72bfb5ef7b
hwanth85@gmail.com <envy0120>
2017-03-04 14:19:59 +0900

    Initial commit

---
+++ src/main/java/com/sancho/SanchoApplication.java
@@ -0,0 +1,12 @@
+package com.sancho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanchoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanchoApplication.class, args);
+	}
+}
---
+++ src/main/java/com/sancho/index/HomeController.java
@@ -0,0 +1,18 @@
+package com.sancho.index;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author Jason, Moon
+ * @since 2017-03-04.
+ */
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public ModelAndView getHomePage() {
+        return new ModelAndView("home");
+    }
+}
